26467b0ed5f1c191a98c481419f049e6122a4e16
Zach Aysan <zachaysan@gmail.com>
2011-05-09 16:04:30 -0400

    gave up on the stupid guide's way of doing hex grids. Rolled my own that is faster and cleaner.

--- grid.rb
+++ grid.rb
@@ -1,73 +1,55 @@
+# -*- coding: utf-8 -*-
+require 'pp'
 
-class Grid
-  # Take in self so that we don't have to type Grid.new.method everytime we want to call it
-  class << self
-    cos_30_degrees = 0.866025404 # precalc'd to save time
-    @s = 20 # the side lenght
-    @h = @s/2.0 # the verticle measure of the projection of the roof
-    @r = @s*cos_30_degrees # the "radius" of the hgon grid
-    @b = @s * 2.0 # need to rename these (they represent the measurements of the box 
-    @a = @r * 2.0 # around the hexagon. They are useful to calculating things fast
-    def snap(px, py)
-      return even_snapper(px, py) if snaps_even? py
-      odd_snapper(px, py)
-    end
-    
-    private
-
-    # this method needs to be refactored
-    def snaps_even?(py, px)
-      if (py / (@w+@h).to_i).even? # this line might be wrong
-        base = (2*r).to_i / px.to_i
-        px = px - base
-        base = (2*s).to_i / py.to_i
-        py = py - base
-        ratio = py.to_f / h.to_f
-        unless (1 - ratio)*@r < px
-          return "top-left-even-triangle"
-        end
-        if px.to_i / r.to_i > 0
-          #might be right triangle
-          px = px - r 
-          unless (1 - ratio)*@r > px
-            return "top-right-even-triangle"
-          end
-        end
-        return "normal even"
-      end
-      false
-    end
-    def snaps_odd
-      px -= (px.to_i/(2*@r.to_i))*2*@r.to_f
-      # since we know it is odd (because the previous method failed to execute)
-      # we know we need to subtract out a @h and an @s 
-      py -= (@h + @s)
-      #now that we've subtracted out the even layer we need to subtract out the whole hgons 
-      py -= (py.to_i/(2*@s.to_i))*2*@s.to_f
-      
-      # note that since the problem is symetrical we can use a coordinate transform
-      # solve it once then inverse the new px value and solve it again
-      side = :left
-      side = :right if px < @r
-      if side == :left
-        symmetrical_odd_solver(px, py)
-      else
-        symmetrical_odd_solver(px*-1, py)
-      end
-    end
-    def symmetrical_odd_solver
-      return :not_top if py/@h > 0
-      ratio = py.to_f/@h.to_f 
-      return :top if px < (1 - ratio)*@r
-      return :not_top
-    end
-    def even_snapper
-      
-    end
-    def odd_snapper
-      
-    end
+class Hexagon
+  attr_reader :height, :width
+  def initialize(height)
+    @COS_30_DEGREES = 0.866025404
+    #   ___     ___
+    #  /   \     |
+    # 〈    〉  height
+    #  \___/    _|_
+    # 
+    # |width|
+    @height = height
+    @width = 2 * @height / @COS_30_DEGREES
+  end
+  def x_distance_to_next_hex
+    # X   X   X
+    #   X   X
+    # X   X
+    # | |
+    #
+    @COS_30_DEGREES * @height
   end
 end
 
-Grid.foo
+class Grid
+  # Take in self so that we don't have to type Grid.new.method everytime we want to call it
+  attr_reader :grid_points
+  def initialize(options)
+    @grid_points = []
+    default_grid_options = { :x_start_location => 30, 
+      :x_end_location => 400, 
+      :y_start_location => 20, 
+      :y_end_location => 300,
+      :hex_height => 25}
+    
+    @settings = default_grid_options.merge options
+    @hex = Hexagon.new(@settings[:hex_height])
+    make_grid_points
+  end
+  def make_grid_points
+    even = true
+    @settings[:x_start_location].step(@settings[:x_end_location], @hex.x_distance_to_next_hex) do |px_hex|
+      adjusted_y = @settings[:y_start_location]
+      adjusted_y += (@hex.height * 0.5) unless even
+      adjusted_y.step(@settings[:y_end_location], @hex.height) do |py_hex|
+        @grid_points << [px_hex, py_hex]
+      end
+      even = !even
+    end
+    @grid_points
+  end
+end
+
